5bd1ea45edd1948c13922d34dce3a96a31bdd852
Oguz Pastirmaci <oguzp@outlook.com>
2020-03-10 07:41:31 -0700

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-set -x
+set -ex
 
 yum install -y oracle-epel-release-el7 oracle-release-el7
 
+mkdir -p ~/dependencies
 # gcc 5.4.0
 yum -y install gcc-c++ gmp-devel mpfr-devel libmpc-devel
-cd ~
+cd ~/dependencies
 wget https://ftp.gnu.org/gnu/gcc/gcc-5.4.0/gcc-5.4.0.tar.bz2
 tar jxvf gcc-5.4.0.tar.bz2
 mkdir gcc-5.4.0-build
@@ -17,7 +18,7 @@ export PATH=/usr/local/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/lib64:$LD_LIBRARY_PATH
 
 # HDF5 1.8.16
-cd ~
+cd ~/dependencies
 wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8/hdf5-1.8.16/bin/linux-centos7-x86_64-gcc483/hdf5-1.8.16-linux-centos7-x86_64-gcc483-shared.tar.gz
 mkdir -p /opt/hdf5
 cd /opt/hdf5
@@ -28,7 +29,7 @@ cd /opt/hdf5/latest/bin/
 echo 'export PATH=$PATH:/opt/hdf5/latest/bin' | sudo tee /etc/profile.d/hdf5.sh
 
 # GSL 2.4
-cd ~
+cd ~/dependencies
 wget ftp://ftp.gnu.org/gnu/gsl/gsl-2.4.tar.gz
 tar zxvf gsl-2.4.tar.gz
 cd gsl-2.4
@@ -45,7 +46,7 @@ rpm --import https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRO
 yum -y install intel-mkl-2017.4-061.x86_64
 
 # FFTW 2
-cd ~
+cd ~/dependencies
 wget http://www.fftw.org/fftw-2.1.5.tar.gz
 tar zxvf fftw-2.1.5.tar.gz
 cd fftw-2.1.5
@@ -60,9 +61,10 @@ make install
 # CUDA 10
 [ -f /usr/local/cuda/bin/nvcc ] && ln -s /usr/local/cuda/bin/nvcc /usr/bin/nvcc
 
-# OPENMPI 1.10.7
-yum -y install openmpi-devel-1.10.7-5.el7.x86_64
-ln -s /usr/lib64/openmpi/bin/mpicc /usr/bin/mpicc
+# OPENMPI
+ln -s /usr/mpi/gcc/openmpi-3.1.1rc1/bin/mpicc /usr/bin/mpicc
+ln -s /usr/mpi/gcc/openmpi-3.1.1rc1/bin/mpirun /usr/bin/mpirun
+ln -s /usr/mpi/gcc/openmpi-3.1.1rc1/bin/mpiexec /usr/bin/mpiexec
 
 # PYTHON, NUMPY, SCIPY, MATPLOTLIB
 yum -y install python3
