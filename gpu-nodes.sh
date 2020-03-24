b317e94ba0436f64485650c6fd713dc6a8a24738
Oguz Pastirmaci <oguzp@outlook.com>
2020-03-24 12:33:55 -0700

    Update gpu-nodes.sh

--- gpu-nodes.sh
+++ gpu-nodes.sh
@@ -19,6 +19,7 @@ mkdir -p $INSTALL_DIR
 # CUDA
 yum remove -y "*cublas*" "cuda*"
 
+cd $INSTALL_DIR
 wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda_8.0.61_375.26_linux-run
 wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/patches/2/cuda_8.0.61.2_linux-run
 sh cuda_8.0.61_375.26_linux-run --silent
