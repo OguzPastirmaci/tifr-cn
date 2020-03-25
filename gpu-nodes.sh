e87fb682ae8c34625c87d91d6b1b0b8b18d5fc96
Oguz Pastirmaci <oguzp@outlook.com>
2020-03-24 20:51:22 -0700

    Yorick

--- gpu-nodes.sh
+++ gpu-nodes.sh
@@ -134,6 +134,7 @@ python -m pip install --user numpy==1.12.1 scipy==0.19 matplotlib==2.0.2
 python3 -m pip install --user numpy==1.13.1 scipy==0.19.1 matplotlib==2.0.2
 
 # YORICK 2.1.06
+cd $INSTALL_DIR
 wget https://github.com/LLNL/yorick/archive/y_2_1_06.tar.gz
 tar zxvf y_2_1_06.tar.gz
 make install
